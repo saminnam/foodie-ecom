c84de369f99b3a4749705f96eeb28c3608d954c0
Your Name <your.email@example.com>
2025-07-22 22:29:03 +0530

    code updated

--- src/ModalPopup/FoodMenu.jsx
+++ src/ModalPopup/FoodMenu.jsx
@@ -11,13 +11,23 @@ const FoodMenu = ({ showMenuList, setShowMenuList, category, setCategory }) => {
       }`}
     >
       <div className="lg:w-[400px] relative w-full mx-5 p-5 rounded bg-white shadow-xl">
-        <button className="absolute right-5 top-5 text-xl cursor-pointer font-bold" onClick={() => setShowMenuList(false)}>x</button>
+        <button
+          className="absolute right-5 top-5 text-xl cursor-pointer font-bold"
+          onClick={() => setShowMenuList(false)}
+        >
+          x
+        </button>
         {menu_list.map((item, index) => (
-          <div key={index} onClick={() =>
-                  setCategory((prev) =>
-                    prev === item.menu_name ? "All" : item.menu_name
-                  )
-                } className="flex gap-5 hover:bg-slate-100 transition-all duration-300 ease-linear cursor-pointer items-center mb-3">
+          <div
+            key={index}
+            onClick={() => {
+              setCategory((prev) =>
+                prev === item.menu_name ? "All" : item.menu_name
+              );
+              setShowMenuList(false);
+            }}
+            className="flex gap-5 hover:bg-slate-100 transition-all duration-300 ease-linear cursor-pointer items-center mb-3"
+          >
             <img
               src={item.menu_img}
               alt={item.menu_name}
